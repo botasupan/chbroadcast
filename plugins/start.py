c9c4288112f319a0c28bbf4253cdaf4fe9b4e496
botasupan <87362466+botasupan@users.noreply.github.com>
2022-02-05 00:16:35 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -114,7 +114,7 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(
-                "GABUNG DULU DISINI",
+                "Join Channel",
                 url = client.invitelink)
         ]
     ]
@@ -122,7 +122,7 @@ async def not_joined(client: Client, message: Message):
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text = 'DAPATKAN VIDEO',
+                    text = 'Try Again',
                     url = f"https://t.me/{client.username}?start={message.command[1]}"
                 )
             ]
